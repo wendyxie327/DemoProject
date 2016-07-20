4b98095762e8510c57702b8d851d81aa88a074e0
wendyxie327 <wendyxie327@163.com>
2016-07-20 22:25:01 +0800

    修改进程图形

--- app/src/main/java/com/wendy/demoproject/customView/widgets/CirProgressView.java
+++ app/src/main/java/com/wendy/demoproject/customView/widgets/CirProgressView.java
@@ -7,8 +7,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
+import com.eagle.androidlib.utils.DensityUtil;
 import com.eagle.androidlib.utils.Logger;
 import com.wendy.demoproject.R;
 
@@ -17,13 +19,14 @@ import com.wendy.demoproject.R;
  */
 public class CirProgressView extends View {
 
-    private int mFirstColor;
-    private int mSecondColor;
-    private int mCircleWidth;// 圆环的宽度
+    private int mFirstColor = Color.RED;
+    private int mSecondColor = Color.BLUE;
+    private int mCircleWidth = DensityUtil.dp2px(getContext(),16);// 圆环的宽度
     private Paint mPaint;
     private int mProgress;
-    private int mSpeed;//速度
+    private int mSpeed = 20;//速度
     private boolean isNext = false;//是否为第二个循环
+    private boolean isFirst = true;//当view第一次显示时，可开启线程
 
 
     public CirProgressView(Context context) {
@@ -37,33 +40,36 @@ public class CirProgressView extends View {
     public CirProgressView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CirProgressView, defStyle, 0);
-        int len = typedArray.length();
-        Logger.d(getContext(), "len=" + len + "---" + typedArray.getIndexCount());
-        for (int i = 0; i < len; i++) {
-            int attr = typedArray.getIndex(i);
-            switch (attr) {
-                case R.styleable.CirProgressView_firstColor:
-                    this.mFirstColor = typedArray.getColor(attr, Color.RED);
-                    break;
-                case R.styleable.CirProgressView_secondColor:
-                    this.mSecondColor = typedArray.getColor(attr, Color.BLUE);
-                    break;
-                case R.styleable.CirProgressView_circleWidth:
-                    this.mCircleWidth = typedArray.getDimensionPixelSize(attr, 16);
-                    break;
-                case R.styleable.CirProgressView_speed:
-                    this.mSpeed = typedArray.getInt(attr, 20);
-                    break;
+        int len = typedArray.getIndexCount();//已定义的内容有几个
+
+        if (len != 0){
+            for (int i = 0; i < len; i++) {
+                int attr = typedArray.getIndex(i);
+                switch (attr) {
+                    case R.styleable.CirProgressView_firstColor:
+                        this.mFirstColor = typedArray.getColor(attr, Color.RED);
+                        break;
+                    case R.styleable.CirProgressView_secondColor:
+                        this.mSecondColor = typedArray.getColor(attr, Color.BLUE);
+                        break;
+                    case R.styleable.CirProgressView_circleWidth:
+                        this.mCircleWidth = typedArray.getDimensionPixelSize(attr, DensityUtil.dp2px(getContext(),16));
+                        break;
+                    case R.styleable.CirProgressView_speed:
+                        this.mSpeed = typedArray.getInt(attr, 20);
+                        break;
+                }
             }
         }
-        Logger.d(getContext(),"speed="+mSpeed);
+
         typedArray.recycle();//清除内存
         mPaint = new Paint();
         // 绘图线程
         new Thread() {
             public void run() {
-                while (true) {
-                    mProgress+=1;
+                while (isFirst || isShown()) {//防止界面关闭，进程不停止
+                    isFirst = false;
+                    mProgress++;
                     if (mProgress >= 360) {
                         mProgress = 0;
                         if (!isNext)
@@ -71,8 +77,7 @@ public class CirProgressView extends View {
                         else
                             isNext = false;
                     }
-                    Logger.d(getContext(),"mProgress="+mProgress);
-                    postInvalidate();
+                    postInvalidate();//刷新view
                     try {
                         Thread.sleep(mSpeed);
                     } catch (InterruptedException e) {
@@ -109,6 +114,5 @@ public class CirProgressView extends View {
             mPaint.setColor(mFirstColor);
             canvas.drawArc(rectf, -90, mProgress, false, mPaint);//圆弧
         }
-        Logger.d(getContext(), "progress=" + mProgress);
     }
 }
