92a0b8056005694506111f463713983360189360
wendyxie <wendyxie327@163.com>
2016-11-06 21:05:44 +0800

    尝试禁止下拉状态栏

--- app/src/main/java/com/wendy/demoproject/MainActivity.java
+++ app/src/main/java/com/wendy/demoproject/MainActivity.java
@@ -1,13 +1,17 @@
 package com.wendy.demoproject;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
 import com.wendy.demoproject.customView.ui.CustomViewActivity;
+
+import java.lang.reflect.Method;
 
 
 /**
@@ -20,20 +24,41 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
 
         String[] items = {"自定义View"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         this.setListAdapter(adapter);
 
         getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position){
+                switch (position) {
                     case 0:
                         startActivity(new Intent(MainActivity.this, CustomViewActivity.class));
                         break;
                 }
             }
         });
+
+        WindowManager.LayoutParams attrs = getWindow().getAttributes();
+        attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        getWindow().setAttributes(attrs);
     }
 
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        disableStatusBar();
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    public void disableStatusBar() {
+        try {
+            Object service = getSystemService("statusbar");
+            Class<?> claz = Class.forName("android.app.StatusBarManager");
+            Method expand = claz.getMethod("collapse");
+            expand.invoke(service);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
